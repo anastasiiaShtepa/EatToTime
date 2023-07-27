9d756d559328a9ddd6fe9e376873f9b341ea2762
Anastasiia Shtepa <a.shtepa@zazmic.com>
2023-07-27 16:16:57 +0200

    added infrastucture

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { Text } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 
 const RestaurantCard = styled(Card)`
-  background-color: white;
+  background-color: ${(props) => props.theme.colors.bg.lavanda};
 `;
 const RestaurantCardCover = styled(Card.Cover)`
-  background-color: green;
-  padding: 20px;
+  background-color: ${(props) => props.theme.colors.bg.lavanda};
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const Title = styled(Text)`
-  padding: 16px;
-  color: red;
+  padding: ${(props) => props.theme.space[3]};
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -29,17 +29,9 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     isClosedTemporarily,
   } = restaurant;
   return (
-    <RestaurantCard elevation={5} style={styles.card}>
-      <RestaurantCardCover
-        key={name}
-        style={styles.cover}
-        source={{ url: photos[0] }}
-      />
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ url: photos[0] }} />
       <Title>{name}</Title>
     </RestaurantCard>
   );
 };
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-});
